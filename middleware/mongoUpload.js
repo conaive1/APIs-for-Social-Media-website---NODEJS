fe3ded5cf67cee09db28db57fe4c482c75cb2ded
conaive1 <54533936+conaive1@users.noreply.github.com>
2021-03-01 17:00:57 +0700

    Add files via upload

--- middleware/mongoUpload.js
+++ middleware/mongoUpload.js
@@ -3,6 +3,15 @@ const crypto = require("crypto");
 const path = require("path");
 const GridFsStorage = require("multer-gridfs-storage");
 
+const fileFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    return cb(
+      new Error(`do not support ${path.extname(file.originalname)}`),
+      false
+    );
+  }
+  cb(null, true);
+};
 var storage = new GridFsStorage({
   url: "mongodb://localhost:27017/mangxahoi",
   file: (req, file) => {
@@ -21,5 +30,5 @@ var storage = new GridFsStorage({
     });
   },
 });
-const Mongoupload = multer({ storage });
+const Mongoupload = multer({ storage, fileFilter });
 exports.Mongoupload = Mongoupload;
